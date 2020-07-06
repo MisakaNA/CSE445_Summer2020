03fb3a59b428ef69e49ffccc9f868f8fb746890c
Kotori Misaka <51569785+MisakaNA@users.noreply.github.com>
2020-07-06 11:58:06 -0700

    Two required services developed

---
+++ Project_03/RequiredServices/IService1.cs
@@ -0,0 +1,18 @@
+﻿using System.ServiceModel;
+using System.ServiceModel.Web;
+
+namespace RequiredServices
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IService1
+    {
+        [OperationContract]
+        [WebGet(UriTemplate="Covid19Cases?country={country}")]
+        string Covid19Cases(string country);
+
+        [OperationContract]
+        [WebGet(UriTemplate = "Weather5Days?city={city}&state={state}&unit={unit}")]
+        string Weather5Days(string city, string state, string unit);
+    }
+}
