72e00295787ec4809265f220e4e8d0c3efb9dc47
Kotori Misaka <51569785+MisakaNA@users.noreply.github.com>
2020-08-18 18:15:06 -0700

    dll for the application

---
+++ Project_05/Encryptor/CryptDLL.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Encryptor
+{
+    class CryptDLL
+    {
+        public string Enrypt(string str)
+        {
+            string KEY = "CSE445Proj5";
+            string encryptedStr = "";
+            for(int i = 0; i < str.Length; i++)
+            {
+                encryptedStr += (char)(str[i] ^ KEY[i % KEY.Length]);
+            }
+
+            return encryptedStr;
+        }
+
+        public string Decrypt(string encryptedStr)
+        {
+            string KEY = "CSE445Proj5";
+            string normalStr = "";
+            for (int i = 0; i < encryptedStr.Length; i++)
+            {
+                normalStr += (char)(encryptedStr[i] ^ KEY[i % KEY.Length]);
+            }
+
+            return normalStr;
+        }
+    }
+}
