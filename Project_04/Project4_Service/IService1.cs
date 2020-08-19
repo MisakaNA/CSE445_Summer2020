5e30ed5668ec3a73d9ec3e581b624c16b0bcbf3b
Kotori Misaka <51569785+MisakaNA@users.noreply.github.com>
2020-08-18 18:00:31 -0700

    Web services

---
+++ Project_04/Project4_Service/IService1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using System.Xml;
+
+namespace Project4_Service
+{
+    [ServiceContract]
+    public interface IService1
+    {
+
+        [OperationContract]
+        [WebGet(UriTemplate = "Verification?xml={xml}&xsd={xsd}")]
+        string Verification(string xml, string xsd);
+    }
+}
