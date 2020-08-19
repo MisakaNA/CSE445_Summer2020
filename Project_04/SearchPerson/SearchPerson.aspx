eff4849c31eb721a916290ce11b4820fe2363faa
Kotori Misaka <51569785+MisakaNA@users.noreply.github.com>
2020-08-18 18:08:26 -0700

    code for search a person in xml

---
+++ Project_04/SearchPerson/SearchPerson.aspx
@@ -0,0 +1,41 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="SearchPerson.aspx.cs" Inherits="WebApp.SearchPerson" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="Label5" runat="server" Font-Bold="True" Font-Size="X-Large" Text="Search Person"></asp:Label>
+            <br />
+        </div>
+        <asp:Label ID="Label1" runat="server" Text="Enter the XML URL: " Font-Size="Medium"></asp:Label>
+        <asp:TextBox ID="xmlURL" runat="server" Font-Size="Small" Width="301px">http://webstrar63.fulton.asu.edu/page9/Persons.xml</asp:TextBox>
+        <asp:Label ID="Label6" runat="server" Text="(Default set as my XML file)"></asp:Label>
+        <br />
+        <br />
+        <asp:Label ID="Label2" runat="server" Text="Enter the person's first name: " Font-Size="Medium"></asp:Label>
+        <asp:TextBox ID="firstName" runat="server" Font-Size="Medium"></asp:TextBox>
+        <br />
+        <br />
+        <asp:Label ID="Label3" runat="server" Text="Enter the person's last name: " Font-Size="Medium"></asp:Label>
+        <asp:TextBox ID="lastName" runat="server" Font-Size="Medium"></asp:TextBox>
+        <br />
+        <br />
+        <br />
+        <asp:Button ID="Search" runat="server" OnClick="Search_Click" Text="Search" Font-Size="Medium" />
+&nbsp;&nbsp;&nbsp;
+        <asp:Label ID="error" runat="server" Font-Bold="True" ForeColor="Red" Font-Size="Medium"></asp:Label>
+        <br />
+        <br />
+        <br />
+        <asp:Label ID="Label4" runat="server" Font-Bold="True" Text="Searching Result:" Font-Size="Medium"></asp:Label>
+        <br />
+        <br />
+        <asp:Label ID="Result" runat="server" Font-Size="Medium"></asp:Label>
+    </form>
+</body>
+</html>
---
+++ Project_04/SearchPerson/SearchPerson.aspx.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text.RegularExpressions;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApp
+{
+    public partial class SearchPerson : System.Web.UI.Page
+    {
+        protected void Search_Click(object sender, EventArgs e)
+        {
+            Regex checkName = new Regex(@"^[a-zA-Z]+$");
+            if(!checkName.IsMatch(firstName.Text) || !checkName.IsMatch(lastName.Text))
+            {
+                error.Text = "Illegal character is detected in input name!";
+                return;
+            }
+            string url = "http://localhost:64999/Service1.svc/SearchPerson?xmlURL=" + xmlURL.Text + 
+                            "&personName=" + firstName.Text + " " + lastName.Text;
+
+            try
+            {
+                var request = (HttpWebRequest)WebRequest.Create(url);
+                var res = request.GetResponse();
+                Result.Text = "";
+                using (var stream = res.GetResponseStream())
+                {
+                    StreamReader reader = new StreamReader(stream);
+                    string dataStr = reader.ReadToEnd();
+                    Regex regex = new Regex(@">.*?<");
+                    dataStr = regex.Match(dataStr).ToString();
+                    if(dataStr == "")
+                    {
+                        Result.Text = "<p style=\"font-weight: bold; Color: Red\">No person named \"" + 
+                                        firstName.Text + " " + lastName.Text + "\" in the current XML file!</p>";
+                        return;
+                    }
+                    dataStr = dataStr.Substring(1, dataStr.Length - 2);
+
+                    var personInfo = dataStr.Split(',');
+
+                    Result.Text = string.Format("Name:&emsp;{0}<br /><br />ID:&emsp;{1}<br /><br />", personInfo[0], personInfo[1]); 
+
+                    if(personInfo[3] == "yes")
+                    {
+                        Result.Text += "Decrypted Password:&emsp;" + Decryptor(personInfo[2]) + 
+                                        "&emsp;(Encrypted password: " + personInfo[2]  + ")<br /><br />";
+                    }
+                    else
+                    {
+                        Result.Text += "Password:&emsp;" + personInfo[2] + "<br /><br />";
+                    }
+
+                    Result.Text += string.Format("Work Phone:&emsp;{0}<br /><br />Cell Phone:&emsp;{1}<br /><br />" +
+                        "                         Provider:&emsp;{2}<br /><br />Category:&emsp;{3}", 
+                                                    personInfo[4], personInfo[5], (personInfo[6] == "") ? "N/A" : personInfo[6], personInfo[7]);
+                }
+            }
+            catch (Exception)
+            {
+                error.Text = "Unable to load the data!";
+                return;
+            }
+        }
+
+        private string Decryptor(string password)
+        {
+            string temp = "";
+            for (int i = 0; i < password.Length; i++)
+            {
+                temp += (char)(password[i] - 1);
+            }
+
+            return temp;
+        }
+    }
+}
